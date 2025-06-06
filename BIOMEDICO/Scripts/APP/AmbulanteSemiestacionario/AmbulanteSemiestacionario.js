61cba01c16819ee0b2f249668d59a2af48d2084b
Javier Ruiz Mendoza <jjavierruiz91@gmail.com>
2025-06-05 23:27:02 -0500

    Formulario Semiestacionario

---
+++ BIOMEDICO/Scripts/APP/AmbulanteSemiestacionario/AmbulanteSemiestacionario.js
@@ -0,0 +1,356 @@
+﻿var ObjAmbulanteSemiestacionario = {
+    AmbulanteSemiestacionarioSport: {}//{objetos} llaves y [array] corchetes
+
+}
+//var validadorFormDeportista = [];
+var IsUpdate = false;
+var AmbulanteSemiestacionarioData = 0;
+var VerDetalles = 'NO';
+var formCitas = [];
+$(document).ready(function () {//FUNCION INICIAL;
+    let DocumentoAmbulanteSemiestacionario = getQueryVariable('Document');
+    VerDetalles = getQueryVariable('Viewdetail');
+    AmbulanteSemiestacionarioData = getQueryVariable('IdReg');
+    //Get_DataGet(CargarSelectSucursales, '/Sucursal/GetListSucursalesPasaporte');
+
+    if (DocumentoAmbulanteSemiestacionario > 0) {
+        IsUpdate = true;
+        CargarInfoCita(DocumentoAmbulanteSemiestacionario);
+    }
+
+    //initValidador();
+
+});
+
+//function initValidador() {
+//    formCitas = Validador("FormCitasMedicasDeportiva2", {
+//        NumeroDocumento: {
+//            required: true,
+//            StringEmpty: true
+//        }
+//    }
+//    );
+//}
+
+//function ValidarCedula() {
+//    let Cedula = $('#NumeroDocumento').val();
+//    Get_Data(MostrarAlerta, '/InformeAccidenteTrabajo/BuscarCedulaPass?Identificacion=' + Cedula)
+//}
+
+
+//function MostrarAlerta(data) {
+
+//    if (data != null || data != undefined) {
+//        swal({
+//            title: "El usuario ya tiene un registro en el formulario.!",
+//            text: "No se pueden diligenciar dos formulario",
+//            type: "warning",
+//            /*showCancelButton: true,*/
+//            confirmButtonColor: "#DD6B55",
+//            confirmButtonText: "Gracias por su visita",
+//           /* cancelButtonText: "No, jamás",*/
+//            closeOnConfirm: false,
+//            closeOnCancel: false
+            
+//        },
+         
+//            function (isConfirm) {
+//                if (isConfirm) {
+//                    swal("¡Gobernacion del cesar!",
+//                        "Gracias por utilizar nuestros servicios",
+//                        "success");
+//                    timer: 90000000
+                    
+//                } else {
+//                    swal("¡Gobernacion del cesar!",
+//                        "Gracias por utilizar nuestros servicios",
+//                        "error");
+                    
+//                }
+//                window.location.href = 'https://localhost:44379/InformeAccidenteTrabajo/Agregar';
+//            });
+        
+////                
+//    }
+    
+//}
+
+    
+
+
+
+//function CargarInfoCita(Documento) {
+//    Get_Data(LlenarDatosformularioInformeAccidenteTrabajo, '/InformeAccidenteTrabajo/BuscarCitas?Ducumento=' + Documento)
+//}
+
+//function LlenarDatosformularioInformeAccidenteTrabajo(data) {
+//    $('#TipoSolicitudPasaporte').val(data.objeto.TipoSolicitudPasaporte);
+//    $('#TipoSolicitudPasaporte')[0].disabled = true;
+//    $('#TipoDocumentoPasaporte').val(data.objeto.TipoDocumentoPasaporte);
+//    $('#TipoDocumentoPasaporte')[0].disabled = true;
+//    $('#NumeroDocumento').val(data.objeto.NumeroDocumento);
+//    $('#NumeroDocumento')[0].disabled = true;
+//    $('#TipoPasaporte').val(data.objeto.TipoPasaporte);
+//    $('#TipoPasaporte')[0].disabled = true;
+//    $('#FechaExpedicionDocumento').val(JSONDateconverter(data.objeto.FechaExpedicionDocumento));
+//    $('#FechaExpedicionDocumento')[0].disabled = true;
+
+//    $('#NombresPasaporte').val(data.objeto.NombresPasaporte);
+//    $('#NombresPasaporte')[0].disabled = true;
+//    $('#ApellidosPasaporte').val(data.objeto.ApellidosPasaporte);
+//    $('#ApellidosPasaporte')[0].disabled = true;
+//    $('#CelularPasaporte').val(data.objeto.CelularPasaporte);
+//    $('#CelularPasaporte')[0].disabled = true;
+//    $('#CorreoPasaporte').val(data.objeto.CorreoPasaporte);
+//    $('#CorreoPasaporte')[0].disabled = true;
+//    $('#CorreoPasaporteRepeated').val(data.objeto.CorreoPasaporteRepeated);
+//    $('#CorreoPasaporteRepeated')[0].disabled = true;
+//    $('#ParentescoMenor').val(data.objeto.ParentescoMenor);
+//    $('#ParentescoMenor')[0].disabled = true;
+//    $('#CuantosMenores').val(data.objeto.CuantosMenores);
+//    $('#CuantosMenores')[0].disabled = true;
+//    IdAmbulanteSemiestacionarioData = data.objeto.IdAmbulanteSemiestacionario;
+//}
+
+
+//function CargarInfoinicial() {
+//    var ValueCitaPasaporte = $('#NumeroDocumento').val();
+//    Get_Data(LlenarcamposInicial, '/InformeAccidenteTrabajo/BuscarCitas?Ducumento=' + ValueCitaPasaporte)
+//}
+
+
+
+//function LlenarcamposInicial(data) {
+
+//    if (data.objeto == null) {
+
+//        /*swal("Good job!", "You clicked the button!", "success");*/
+//        swal({
+//            title: "Oficina Politica Social",
+//            text: "Usted no tiene registros!",
+//            type: "warning",
+//            confirmButtonColor: "#DD6B55",
+//            confirmButtonText: "Muchas Gacias!",
+
+
+//        });
+
+//        return;
+
+
+
+//    }
+//    $('#TipoSolicitudPasaporte').val(data.objeto.TipoSolicitudPasaporte);
+//    $('#TipoDocumentoPasaporte').val(data.objeto.TipoDocumentoPasaporte);
+//    $('#NumIdentificacion').val(data.objeto.NumIdentificacion);
+//    $('#Fecha').val(JSONDateconverter(data.objeto.Fecha));
+//    $('#Hora').val(data.objeto.Hora +" : " + data.objeto.Minutos); 
+//    /*$('#Minutos').val(data.objeto.Minutos);*/
+   
+    
+//    $('#NombresPasaporte').val(data.objeto.NombresPasaporte);
+//    $('#ApellidosPasaporte').val(data.objeto.ApellidosPasaporte);
+//    $('#CelularPasaporte').val(data.objeto.CelularPasaporte);
+//    $('#CorreoPasaporte').val(data.objeto.CorreoPasaporte);
+    
+   
+//    $('#IdCitas').val(data.objeto.IdAmbulanteSemiestacionario);
+//    Alternar(ConsultaPasaporte);
+//}
+
+//function LlenarCampos(data) {
+//    $('#NombresPasaporte').val(data.objeto.NombresPasaporte);
+//    $('#ApellidosPasaporte').val(data.objeto.ApellidosPasaporte);
+
+//    CargarInfoinicial();
+//}
+
+function getQueryVariable(variable) {//saca los valores de la uRL
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    return 0;
+}
+
+function Atras() {
+    window.history.back();
+}
+
+function Createobj() {
+
+    
+
+        // if (validadorFormMedicinaDeportiva.form()) {
+        if (VerDetalles == "SI") {
+            Atras();
+        }
+        else {
+
+            var IdAmbulanteSemiestacionario = 0;
+            if (IsUpdate) {
+                IdAmbulanteSemiestacionario = AmbulanteSemiestacionarioData;
+            }
+            ObjAmbulanteSemiestacionario = {
+                AmbulanteSemiestacionarioSport: {
+
+
+                    IdAmbulanteSemiestacionario: IdAmbulanteSemiestacionario,
+                    FechaAmbulanteSemiestacionario: $('#FechaAmbulanteSemiestacionario').val(),
+                    FormaTrabajoAmbulanteAmbulanteSemiestacionario: $('#FormaTrabajoAmbulanteAmbulanteSemiestacionario').val(),
+                    MunicipicioAmbulanteSemiestacionario: $('#MunicipicioAmbulanteSemiestacionario').val(),
+                    AmbitoTerritorialAmbulanteSemiestacionario: $('#AmbitoTerritorialAmbulanteSemiestacionario').val(),
+                    NombresAmbulanteSemiestacionario: $('#NombresAmbulanteSemiestacionario').val(),
+                    ApellidosAmbulanteSemiestacionario: $('#ApellidosAmbulanteSemiestacionario').val(),
+                    TipoIdentificacionAmbulanteSemiestacionario: $('#TipoIdentificacionAmbulanteSemiestacionario').val(),
+                    NumeroIDAmbulanteSemiestacionario: $('#NumeroIDAmbulanteSemiestacionario').val(),
+                    FechaNacimientoAmbulanteSemiestacionario: $('#FechaNacimientoAmbulanteSemiestacionario').val(),
+                    OficioAmbulanteSemiestacionario: $('#OficioAmbulanteSemiestacionario').val(),
+                    SexoAmbulanteSemiestacionario: $('#SexoAmbulanteSemiestacionario').val(),
+                    EducacionAmbulanteSemiestacionario: $('#EducacionAmbulanteSemiestacionario').val(),
+                    PoblacionAmbulanteSemiestacionario: $('#PoblacionAmbulanteSemiestacionario').val(),
+                    CondicionAmbulanteSemiestacionario: $('#CondicionAmbulanteSemiestacionario').val(),
+                    OrgnizacionSocialAmbulanteSemiestacionario: $('#OrgnizacionSocialAmbulanteSemiestacionario').val(),
+                    SeguridadSocialAmbulanteSemiestacionario: $('#SeguridadSocialAmbulanteSemiestacionario').val(),
+                    AntiguedadTrabajoAmbulanteSemiestacionario: $('#AntiguedadTrabajoAmbulanteSemiestacionario').val(),
+                    JornadaAmbulanteSemiestacionario: $('#JornadaAmbulanteSemiestacionario').val(),
+                    HorasDiasAmbulanteSemiestacionario: $('#HorasDiasAmbulanteSemiestacionario').val(),
+                    DiasSemanaAmbulanteSemiestacionario: $('#DiasSemanaAmbulanteSemiestacionario').val(),
+                    DesplazamientoAmbulanteSemiestacionario: $('#DesplazamientoAmbulanteSemiestacionario').val(),
+                    EnfermedadAmbulanteSemiestacionario: $('#EnfermedadAmbulanteSemiestacionario').val(),
+                    SustaniaAmbulanteSemiestacionario: $('#SustaniaAmbulanteSemiestacionario').val(),
+                    AlcoholAmbulanteSemiestacionario: $('#AlcoholAmbulanteSemiestacionario').val(),
+                    ActividadFisicaAmbulanteSemiestacionario: $('#ActividadFisicaAmbulanteSemiestacionario').val(),
+                    HorasDuermeAmbulanteSemiestacionario: $('#HorasDuermeAmbulanteSemiestacionario').val(),
+                    FrutasVerdurasAmbulanteSemiestacionario: $('#FrutasVerdurasAmbulanteSemiestacionario').val(),
+                    ComportamientoAmbulanteSemiestacionario: $('#ComportamientoAmbulanteSemiestacionario').val(),
+                    SintomasAmbulanteSemiestacionario: $('#SintomasAmbulanteSemiestacionario').val(),
+                    AcidenteAmbulanteSemiestacionario: $('#AcidenteAmbulanteSemiestacionario').val(),
+                    DiscapacidadAmbulanteSemiestacionario: $('#DiscapacidadAmbulanteSemiestacionario').val(),
+                    SeguridadAmbulanteSemiestacionario: $('#SeguridadAmbulanteSemiestacionario').val(),
+                    PeligroFisicoAmbulanteSemiestacionario: $('#PeligroFisicoAmbulanteSemiestacionario').val(),
+                    PeligroBiomecanicoAmbulanteSemiestacionario: $('#PeligroBiomecanicoAmbulanteSemiestacionario').val(),
+                    PeligroQuimicoAmbulanteSemiestacionario: $('#PeligroQuimicoAmbulanteSemiestacionario').val(),
+                    PeligroBiologicoAmbulanteSemiestacionario: $('#PeligroBiologicoAmbulanteSemiestacionario').val(),
+                    PeligroPsicosocialAmbulanteSemiestacionario: $('#PeligroPsicosocialAmbulanteSemiestacionario').val(),
+                    AguaAmbulanteSemiestacionario: $('#AguaAmbulanteSemiestacionario').val(),
+                    ExcretaAmbulanteSemiestacionario: $('#ExcretaAmbulanteSemiestacionario').val(),
+                    ResiduosAmbulanteSemiestacionario: $('#ResiduosAmbulanteSemiestacionario').val(),
+                    CombustibleAmbulanteSemiestacionario: $('#CombustibleAmbulanteSemiestacionario').val()
+                    
+                    
+
+       
+        
+
+
+                }
+            }
+            let id = 10;
+
+            if (IsUpdate) {
+                swal({
+                    title: "Atención",
+                    text: "¿Estas seguro de actualizar el formulario ?",
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonClass: "btn-danger",
+                    confirmButtonText: "Si",
+                    cancelButtonText: "No",
+                    closeOnConfirm: false,
+                    closeOnCancel: false
+                },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            swal.close()
+                            Save_Data(ActualizarVista, '/AmbulanteSemiestacionario/EditarInformeAccidenteTrabajo', ObjAmbulanteSemiestacionario, 'Actualizacion');
+                            /*window.location.href = 'http://127.0.0.1:5501/index.html';*/
+                        }
+                        else {
+                            swal.close()
+                        }
+                    });
+
+
+            }
+            else {
+                Save_Data(ActualizarVista, '/AmbulanteSemiestacionario/Agregar', ObjAmbulanteSemiestacionario, 'Guardado');
+                /*window.location.href = 'http://127.0.0.1:5501/index.html';*/
+
+                // LimpiarFormulario()
+            }
+
+            //} else {
+            //    SwalErrorMsj("No ingreso todos los campos por favor verifique");
+            //}
+
+        
+
+    }
+}
+function ActualizarVista() {
+    window.location.reload();
+}
+
+//function RenderUpdateCita(viewfree) {
+
+//    window.location.href = "../InformeAccidenteTrabajo/agregar?ViewFree=" + viewfree + "&Document=" + $('#NumeroDocumento').val();
+
+//}
+
+//function CancelarCita() {
+//    swal({
+//        title: "Atención",
+//        text: "¿Estas seguro de cancelar la cita ?",
+//        type: "warning",
+//        showCancelButton: true,
+//        confirmButtonClass: "btn-danger",
+//        confirmButtonText: "Si",
+//        cancelButtonText: "No",
+//        closeOnConfirm: false,
+//        closeOnCancel: false
+//    },
+//        function (isConfirm) {
+//            if (isConfirm) {
+//                swal.close()
+//                Get_Data(reloadPage, "/InformeAccidenteTrabajo/ActualizarEstadoCancelar?IdCitaPasaporte=" + $('#IdCitas').val())
+//            }
+//            else {
+//                swal.close()
+//            }
+//        });
+
+
+//}
+
+
+function reloadPage() {
+    window.location.reload();
+}
+
+
+//function LimpiarFormulario() {
+
+//    $('#IdAmbulanteSemiestacionario').val('')
+//    $('#Sucursales').val('')
+//    $('#FechaCalen').val('')
+//    $('#Hora').val('')
+//    $('#Minutos').val(''),
+//        $('#Segundos').val('')
+
+
+//}
+
+
+//function validarCorreo() {
+//    var correoElectronico = document.getElementById("CorreoElectronico").value;
+//    var expresionRegular = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+//    if (!expresionRegular.test(correoElectronico)) {
+//        alert("Por favor, ingrese un correo electrónico válido.");
+//    }
+//}
---
+++ BIOMEDICO/Scripts/APP/AmbulanteSemiestacionario/ListaAmbulanteSemiestacionarioInforme.js
@@ -0,0 +1,164 @@
+﻿
+
+var TablaAmbulanteSemiestacionario = [];
+$(document).ready(function () {
+
+    RenderTable('datatable-AmbulanteSemiestacionario', [0, 1, 2, 3, 4, 5,6], null, {
+        "paging": true,
+        "ordering": false,
+        "info": true,
+        "searching": true,
+
+        "dom": '<"top"flB>rt<"bottom"ip><"clear">',
+        //dom: 'frtip',
+
+        buttons: [
+            {
+                extend: 'excelHtml5',
+                text: " <b>&ensp;<i class=' icon-download4 position-left'></i></b> Excel ",
+                filename: "InformeAccidente",
+                titleAttr: 'Excel',
+            },
+            {
+                extend: 'pdfHtml5',
+                text: " <b>&ensp;<i class=' icon-download4 position-left'></i></b> PDF ",
+                filename: "InformeAccidente",
+                titleAttr: 'Pdf',
+            },
+
+        ]
+    });
+
+
+    TablaAmbulanteSemiestacionario = $('#datatable-AmbulanteSemiestacionario').DataTable();
+    Get_Data(CargarTabla, '/AmbulanteSemiestacionario/GetListAmbulanteSemiestacionario')
+
+});
+var Arraycitasglobal = [];
+function CargarTabla(data) {
+    TablaAmbulanteSemiestacionario.clear().draw();
+    let AmbulanteSemiestacionarioSport = data.objeto;
+    Arraycitasglobal = AmbulanteSemiestacionarioSport;
+    console.log(AmbulanteSemiestacionarioSport);
+    $.each(AmbulanteSemiestacionarioSport, function (index, item) {
+       
+            TablaAmbulanteSemiestacionario.row.add([
+                /* item.IdCitasPasaporte,*/
+                item.NombresAmbulanteSemiestacionario,
+                item.ApellidosAmbulanteSemiestacionario,
+                item.MunicipicioAmbulanteSemiestacionario,
+                item.FormaTrabajoAmbulanteAmbulanteSemiestacionario,
+                //item.FechaNacimiento == undefined ? '' : JSONDateconverter(item.FechaNacimiento),
+                item.TipoIdentificacionAmbulanteSemiestacionario,
+                item.NumeroIDAmbulanteSemiestacionario,
+
+
+
+                '<i class="btn btn-danger btn-group-sm icon-trash" title="Eliminar" onclick="Eliminar(' + item.IdAmbulanteSemiestacionario + ')" ></i>&ensp;' +
+                '<i class="btn btn-primary btn-group-sm fa fa-pencil-square-o" id="edit_ActEco_' + index + '" title="Modificar" style="fontsize:90px !important" onclick="ActualizardPoliticaSocial(' + item.IdAmbulanteSemiestacionario + ')"></i>&ensp;' +
+                '<i class="btn btn-info btn-group-sm icon-magazine" title="Detalle" onclick="DetalleData(' + item.IdAmbulanteSemiestacionario + ')" ></i>&ensp;'
+            ]).draw(false);
+
+            //var Discapacidad = TablaAmbulanteSemiestacionario.column(9);
+            //var GrupoEtnico = TablaAmbulanteSemiestacionario.column(10);
+            //var VictimaConflictoArmado = TablaAmbulanteSemiestacionario.column(11);
+            //var PoblacionPriorizada = TablaAmbulanteSemiestacionario.column(12);
+            //var CorreoElectronico = TablaAmbulanteSemiestacionario.column(13);
+            //var TelefonoContacto = TablaAmbulanteSemiestacionario.column(14);
+            //var Municipios = TablaAmbulanteSemiestacionario.column(15);
+            //var Zona = TablaAmbulanteSemiestacionario.column(16);
+            //var NivelEscolaridad = TablaAmbulanteSemiestacionario.column(17);
+            //var Profesion = TablaAmbulanteSemiestacionario.column(18);
+            //var AmbienteLudico = TablaAmbulanteSemiestacionario.column(19);
+            //var AmbienteLudicoExpresa = TablaAmbulanteSemiestacionario.column(20);
+            //var AmbienteLudicoCorporal = TablaAmbulanteSemiestacionario.column(21);
+            //var FechaRegistro = TablaAmbulanteSemiestacionario.column(22);
+
+        
+        //Discapacidad.visible(false);
+        // GrupoEtnico.visible(false);
+        //  VictimaConflictoArmado.visible(false);
+        //  PoblacionPriorizada.visible(false);
+        //  CorreoElectronico.visible(false);
+        //  TelefonoContacto.visible(false);
+        //  Municipios.visible(false);
+        //  Zona.visible(false);
+        //  NivelEscolaridad.visible(false);
+        //  Profesion.visible(false);
+        //  AmbienteLudico.visible(false);
+        //  AmbienteLudicoExpresa.visible(false);
+        //  AmbienteLudicoCorporal.visible(false);
+        //  FechaRegistro.visible(false);
+
+        
+        
+    });
+}
+
+
+
+
+
+
+
+function ActualizardPoliticaSocial(idCitasPasport) {
+    window.location.href = '../CitasPasaporte/Agregar?IdCitasPasportReg=' + idCitasPasport +'&IsUpdate=true';
+
+}
+
+
+function DetalleData(idCitasPasport) {
+    window.location.href = '../CitasPasaporte/Agregar?IdCitasPasportReg=' + idCitasPasport + "&Viewdetail=SI";
+
+}
+//function ActualizarEstadoTramitados(idCitasPasport) {
+//    swal({
+//        title: "Atención",
+//        text: "¿Estas seguro de tramitar la cita ?",
+//        type: "warning",
+//        showCancelButton: true,
+//        confirmButtonClass: "btn-danger",
+//        confirmButtonText: "Si",
+//        cancelButtonText: "No",
+//        closeOnConfirm: false,
+//        closeOnCancel: false
+//    },
+//        function (isConfirm) {
+//            if (isConfirm) {
+//                swal.close()
+//                Get_Data(RecargarTabla, '/CitasPasaporte/ActualizarEstadoTramitado?IdCitaPasaporte=' + idCitasPasport);
+//            }
+//            else {
+//                swal.close()
+//            }
+//        });
+//}
+
+
+
+//function ActualizarCitasPasapor(idCitasPasport) {
+//    swal({
+//        title: "Atención",
+//        text: "¿Estas seguro de actualizar la cita ?",
+//        type: "warning",
+//        showCancelButton: true,
+//        confirmButtonClass: "btn-danger",
+//        confirmButtonText: "Si",
+//        cancelButtonText: "No",
+//        closeOnConfirm: false,
+//        closeOnCancel: false
+//    },
+//        function (isConfirm) {
+//            if (isConfirm) {
+//                swal.close()
+//                Get_Data(RecargarTabla, '/CitasPasaporte/EditarCitasPasaporte?IdCitaPasaporte=' + idCitasPasport);
+//            }
+//            else {
+//                swal.close()
+//            }
+//        });
+//}
+
+function RecargarTabla() {
+    Get_Data(CargarTabla, '/InformeAccidenteTrabajo/GetListInformeAccidenteTrabajo')
+}
